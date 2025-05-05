5ba5e05a43f7e77be618a8be87e465caf5f3953d
gaoziyuan <gaoziyuan.955@bytedance.com>
2025-05-05 10:21:41 +0800

    try to test reward model

--- examples/ppo_trainer/run_qwen1.5_moe_a2.7b-gsm8k_megatron.sh
+++ examples/ppo_trainer/run_qwen1.5_moe_a2.7b-gsm8k_megatron.sh
@@ -26,7 +26,7 @@ CP=1
 VLLM_TP=4
 
 # RAY_ADDRESS='auto' ray job submit --working-dir . -- 
-python3 -m verl.trainer.main_ppo --config-path=./config --config-name='ppo_megatron_trainer'\
+python3 -m verl.trainer.main_ppo --config-path=./config --config-name='ppo_megatron_trainer' \
     algorithm.adv_estimator=gae \
     data.train_files="$train_files" \
     data.val_files="$test_files" \
